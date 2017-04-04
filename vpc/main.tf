673277d7f406a6e94e3d9bf7865e41340adedbb9
Tim A Wang <tim.a.wang@pwc.com>
2017-04-04 17:25:42 +1000

    use one nat for all pv subnets

--- vpc/main.tf
+++ vpc/main.tf
@@ -130,7 +130,7 @@ resource "aws_route" "internal" {
   count                  = "${length(compact(var.internal_subnets["subnets"]))}"
   route_table_id         = "${element(aws_route_table.internal.*.id, count.index)}"
   destination_cidr_block = "0.0.0.0/0"
-  nat_gateway_id         = "${aws_nat_gateway.main.0.id}"
+  nat_gateway_id         = "${aws_nat_gateway.main.id}"
 }
 
 /**
