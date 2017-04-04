7073e23b5c335216cae03496ba65792e6c1428a1
Tim A Wang <tim.a.wang@pwc.com>
2017-04-04 17:19:10 +1000

    change counter starting position

--- vpc/main.tf
+++ vpc/main.tf
@@ -56,7 +56,7 @@ resource "aws_internet_gateway" "main" {
 
 resource "aws_nat_gateway" "main" {
   #count         = "${length(var.internal_subnets)}"
-  count         = 1
+  count         = 0
   allocation_id = "${element(aws_eip.nat.*.id, count.index)}"
   subnet_id     = "${element(aws_subnet.external.*.id, count.index)}"
   depends_on    = ["aws_internet_gateway.main"]
@@ -64,7 +64,7 @@ resource "aws_nat_gateway" "main" {
 
 resource "aws_eip" "nat" {
   #count = "${length(var.internal_subnets)}"
-  count = 1
+  count = 0
   vpc   = true
 }
 
